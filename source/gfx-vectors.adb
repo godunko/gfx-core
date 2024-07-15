33dc1d78fb227dbdcc92360eaf3ff563dce18c71
Vadim Godunko <vgodunko@gmail.com>
2024-07-15 09:10:52 +0400

    GFX.Vectors package.

---
+++ source/gfx-vectors.adb
@@ -0,0 +1,31 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+package body GFX.Vectors is
+
+   ---------
+   -- "*" --
+   ---------
+
+   function "*" (Left : GF_Vector; Right : GX_Real) return GF_Vector is
+   begin
+      return (X => Left.X * Right, Y => Left.Y * Right);
+   end "*";
+
+   ---------------
+   -- Normalize --
+   ---------------
+
+   function Normalize (Self : GF_Vector) return GF_Vector is
+      Length : constant Real := Sqrt (Self.X * Self.X + Self.Y * Self.Y);
+
+   begin
+      return (X => Self.X / Length, Y => Self.Y / Length);
+   end Normalize;
+
+end GFX.Vectors;
---
+++ source/gfx-vectors.ads
@@ -0,0 +1,22 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+package GFX.Vectors
+  with Pure
+is
+
+   type GF_Vector is record
+      X : GFX.Real;
+      Y : GFX.Real;
+   end record;
+
+   function "*" (Left : GF_Vector; Right : GX_Real) return GF_Vector;
+
+   function Normalize (Self : GF_Vector) return GF_Vector;
+
+end GFX.Vectors;
